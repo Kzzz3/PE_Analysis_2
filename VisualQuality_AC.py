bea193646f5fe48067690e11df62a1cf83a4f159
kzzz3 <k1910004282@sina.com>
2024-07-23 21:09:26 +0800

    first commit

---
+++ VisualQuality_AC.py
@@ -0,0 +1,47 @@
+import os
+
+from skimage import io
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+
+# AC Encryption
+strImageName = "Baboon.bmp"
+strSrcImgPath = r"C:\Users\kzzz3\Desktop\Result\InputImage"
+strDstImgPath = r"C:\Users\kzzz3\Desktop\Result\OutputImage\AcEncryption"
+
+dictPSNR = {}
+dictSSIM = {}
+strRoot = strDstImgPath
+arrQFs = os.listdir(strDstImgPath)
+for QF in arrQFs:
+    dictPSNR[int(QF[QF.find("=") + 1:])] = {}
+    dictSSIM[int(QF[QF.find("=") + 1:])] = {}
+    strQFRoot = os.path.join(strRoot, QF)
+    arrSTs = os.listdir(strQFRoot)
+    for ST in arrSTs:
+        strSTRoot = os.path.join(strQFRoot, ST)
+
+        strPlainImagePath = os.path.join(strSrcImgPath, strImageName)
+        strCipherImagePath = os.path.join(strSTRoot, strImageName[:strImageName.rfind(".")] + ".jpg")
+
+        # 计算图像psnr 和 ssim
+        imgSrc = io.imread(strPlainImagePath)
+        imgDst = io.imread(strCipherImagePath)
+        psnr = peak_signal_noise_ratio(imgDst, imgSrc)
+        ssim = structural_similarity(imgDst, imgSrc)
+
+        dictPSNR[int(QF[QF.find("=")+1:])][int(ST[ST.find("=")+1:])] = psnr
+        dictSSIM[int(QF[QF.find("=")+1:])][int(ST[ST.find("=")+1:])] = ssim
+
+#draw the result
+arrQFs = [ QF for QF in list(dictPSNR.keys())]
+arrSTs = [ ST for ST in list(dictPSNR.values())[0]]
+arrSTs.sort()
+
+for QF in arrQFs:
+    print("QF="+str(QF))
+    print("PSNR:")
+    for ST in arrSTs:
+        print(ST, round(dictPSNR[QF][ST],3),r"\\")
+    print("SSIM:")
+    for ST in arrSTs:
+        print(ST, round(dictSSIM[QF][ST],3),r"\\")
